2e1e078bb58658ea89a96835dfa82b838eb142b5
McCoughskii <tsirgamesalott+github@gmail.com>
2021-08-24 20:27:53 -0500

    commit on main

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,9 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
-		client.logger.ready(
-			`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
+		client.logger.log(
+			`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`,
+            "ready"
 		);
 	},
 };
